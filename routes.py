0c8cf90e0862d219449084fd0eedbb49e04c0088
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-26 16:35:34 +0200

    Add length check to creating topic or message

--- routes.py
+++ routes.py
@@ -16,6 +16,15 @@ def new_topic():
 def create_topic():
     subject = request.form["subject"]
     message = request.form["content"]
+
+    error = check_subject_errors(subject)
+    if error:
+        return error
+
+    error = check_message_errors(message)
+    if error:
+        return error
+
     topics.create(subject, message)
     return redirect("/") # TODO: fix redirect
 
@@ -28,6 +37,11 @@ def show_topic(topic_id):
 @app.route("/topic/<int:topic_id>/send", methods=["POST"])
 def send_message(topic_id):
     content = request.form["content"]
+
+    error = check_message_errors(content)
+    if error:
+        return error
+
     messages.send(content, topic_id)
     return redirect("/topic/"+str(topic_id))
 
@@ -35,3 +49,19 @@ def send_message(topic_id):
 def delete_message(topic_id, message_id):
     messages.remove(message_id)
     return redirect("/topic/"+str(topic_id))
+
+def check_subject_errors(subject):
+    if len(subject) < 3:
+        return "Liian lyhyt otsikko"
+    if len(subject) > 100:
+        return "Liian pitkä otsikko"
+
+    return None
+
+def check_message_errors(message):
+    if len(message) < 3:
+        return "Liian lyhyt viesti"
+    if len(message) > 5000:
+        return "Liian pitkä viesti"
+
+    return None
